f80980d09027ba16b461b25c59139ed08451fd4f
Miihir79 <mihirrshah02@gmail.com>
2021-07-19 02:07:34 +0530

    Initial commit

---
+++ app/src/main/java/com/mihir/testforlibrary/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.mihir.testforlibrary
+
+import android.graphics.Color.red
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.mihir.drawingcanvas.drawingView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_undo.setOnClickListener {
+            drawing_view.undo()
+        }
+
+        btn_redo.setOnClickListener {
+            drawing_view.redo()
+        }
+
+        btn_color.setOnClickListener {
+            drawing_view.setBrushColor(R.color.teal_200)
+        }
+
+        btn_brush.setOnClickListener {
+            drawing_view.setSizeForBrush(25)//0-35
+            drawing_view.setBrushAlpha(100) //0-255
+        }
+        btn_clearscreen.setOnClickListener {
+            drawing_view.clearDrawingBoard()
+        }
+    }
+}
